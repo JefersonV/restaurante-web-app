a865648efcb9079973ac35b3e1b60132e30051e7
Victor Samines <saminesvictor@gmail.com>
2023-04-26 19:52:02 -0600

    Creacion de ficheros para la implemetacion de register y login con JWT

---
+++ Data/DTOs/UsuarioDtoOut.cs
@@ -0,0 +1,12 @@
+﻿namespace restaurante_web_app.Data.DTOs
+{
+    public class UsuarioDtoOut
+    {
+        public Guid IdUsuario { get; set; }
+        public string Nombre { get; set; } = null!;
+
+        public string Usuario1 { get; set; } = null!;
+
+        public string TipoUsuario { get; set; } = null!;
+    }
+}
