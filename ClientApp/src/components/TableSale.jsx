c9dbf2e94d42001eea994d4c3e61d8c90c354486
JefersonV <jvelasquezc15@miumg.edu.gt>
2023-05-18 08:02:18 -0600

    Ventas -> bug de cantidad arreglado, hace falta hacer validaciones y mejorarlo visualmente

--- ClientApp/src/components/TableSale.jsx
+++ ClientApp/src/components/TableSale.jsx
@@ -1,57 +1,102 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import { Table, Input } from 'reactstrap'
 import dayjs from 'dayjs'
 import Skeleton from 'react-loading-skeleton'
 import { AiFillPlusSquare, AiFillMinusSquare } from 'react-icons/ai'
 import { BsFillTrashFill } from 'react-icons/bs';
 
-function TableSale({noComanda, saleDetail, deleteSaleDetail }) {
-  // 1 por defecto
-  const [cantidades, setCantidades] = useState([1]);
+function TableSale({noComanda, saleDetail, deleteSaleDetail, cantidades, updateCantidades, updateCantidadesDelete }) {
+  const [subtotal, setSubtotal] = useState(0)
   const [totalesPlatillos, setTotalPlatillos] = useState([])
 
   /* ------ Función para manejar el cambio de cantidad de un elemento -------*/
-  const handleCantidadChange = (indice, e) => {
-    // Identifica que input está siendo modificado
-    const nuevaCantidad = parseInt(e.target.value);
-    // Actualiza el array de cantidades
+  useEffect(() => {
+    console.log(cantidades)
+  }, [cantidades])
+
+  useEffect(() => {
+    console.log(saleDetail)
+  }, [saleDetail])
+
+
+
+  /* Nota: el state cantidades está definido en el componente padre, y se recibe como prop */
+  // Manejador del <input /> de cantidad de la tabla, identifica que input está siendo modificado
+  const handleCantidadChange = (index, e) => {
+    // Si no se a ingresado una cantidad, o el usuario la borra antes de eliminar el item, entonces se iguala a ""
+    const nuevaCantidad = e.target.value.trim() === "" || e.target.value === undefined || e.target.value === null ? "" 
+    : // si ingresa correctamente un número, entonces se parsea y agrega el número al array "cantidades"
+    parseInt(e.target.value)
+    // Actualizamos el state con el array de cantidades
+    updateCantidades(index, nuevaCantidad)
+  };
+  
+  // Problema: al eliminar el item del array saleDetail [{props}] se debe de eliminar también la cantidad asociada =[10,2,3 ....] en la tabla
+  // Derivado
+  // Problema 2: cuando el <input /> tiene undefined o null, y se elimina toma el valor que tenía el anterior que fue eliminado, porque el array cantidades solo almacena los que ya tienen definido su value
+  /* Eliminar la cantidad asociada */
+  
+  
+  const handleCantidadDelete = (indexItem) => {
+    // Actualiza la cantidad asociada de la venta, evalúa si está vacía entonces le asigna "", para que aunque esté vacía se elimine junto con su item asociado
+    updateCantidadesDelete(indexItem)
+  }
+
+  /* const handleCantidadDelete = (indexItem) => {
     setCantidades((prevCantidades) => {
-      // Nuevas cantidades es una copia de prevCantidades [2,3,43 ... etc] cada número simboliza el value de cada input
+      const cantidadesActualizadas = [...prevCantidades];
+      cantidadesActualizadas.splice(indexItem, 1); // Elimina la cantidad correspondiente en el mismo índice
+      return cantidadesActualizadas;
+    })} */
+    // Eliminar la cantidad asociada en la tabla
+    /* setCantidades((prevCantidades) => {
+
+      const cantidadesActualizadas = prevCantidades.filter((_,index) => index !== indexItem)
+      console.log(cantidades)
+      return cantidadesActualizadas
+    })
+  } */
+  /* const handleCantidadChangePlus = (index, incremento) => {
+    setCantidades((prevCantidades) => {
       const nuevasCantidades = [...prevCantidades];
-      // Indice representa la posición del elemento en el array de cantidades que queremos modificar.
-      nuevasCantidades[indice] = nuevaCantidad; // nuevaCantidad es el nuevo valor de la cantidad que se ha ingresado en el input
-      console.warn('nuevas')
-      console.log(nuevasCantidades)
-      
+      nuevasCantidades[index] += incremento;
       return nuevasCantidades;
     });
   };
 
-  const handleCantidadChangePlus = (indice, incremento) => {
+  const handleCantidadChangeMinus = (index, incremento) => {
     setCantidades((prevCantidades) => {
       const nuevasCantidades = [...prevCantidades];
-      nuevasCantidades[indice] += incremento;
+      nuevasCantidades[index] -= incremento;
       return nuevasCantidades;
     });
-  };
-
-  const handleCantidadChangeMinus = (indice, incremento) => {
-    setCantidades((prevCantidades) => {
-      const nuevasCantidades = [...prevCantidades];
-      nuevasCantidades[indice] -= incremento;
-      return nuevasCantidades;
-    });
-  };
+  }; */
 
 
-  const handleDelete = (id) => {
+  const handleDelete = (id, index) => {
     console.log(id)
-    deleteSaleDetail(id); // Llama a la función deleteSaleDetail del componente padre
+    // Array que almacena los items seleccionados del componente de búsqueda
+
+    deleteSaleDetail(id); 
+    // Otra función
+    handleCantidadDelete(index)
     console.log(saleDetail)
   };
 
-  console.log(saleDetail)
-  const [totalPlatillo, setTotalPlatillo] = useState(50)
+  // console.log(saleDetail)
+  const [totalPlatillo, setTotalPlatillo] = useState(0)
+
+  /* Cálculo del total de toda la comanda */
+  const totalComanda = useMemo(() => {
+    let total = 0;
+    for (let i = 0; i < saleDetail.length; i++) {
+      const precio = saleDetail[i].precio
+      const cantidad = cantidades[i] || 0
+      total += precio * cantidad
+    }
+    return total
+  }, [saleDetail, cantidades])
+  
   return (
     <Table 
       bordered
@@ -76,7 +121,7 @@ function TableSale({noComanda, saleDetail, deleteSaleDetail }) {
             Precio Unitario
           </th>
           <th>
-            Total
+            Subtotal
           </th>
         </tr>
       </thead>
@@ -104,11 +149,11 @@ function TableSale({noComanda, saleDetail, deleteSaleDetail }) {
             type="number"
             tabIndex={index + 1}
             value={cantidades[index]}
-            onChange={(event) => handleCantidadChange(index, event)}
+            onChange={(e) => handleCantidadChange(index, e)}
           />
             <button 
               className="btn-plus"
-              onClick={() => handleCantidadChangePlus(index, 1)}
+              // onClick={() => handleCantidadChangePlus(index, 1)}
             >
               <AiFillPlusSquare 
                 size={24}
@@ -119,7 +164,7 @@ function TableSale({noComanda, saleDetail, deleteSaleDetail }) {
               <AiFillMinusSquare 
                 size={24}
                 color="#dc3545"
-                onClick={() => handleCantidadChangeMinus(index, 1)}
+                // onClick={() => handleCantidadChangeMinus(index, 1)}
               />
             </button>
           </div>
@@ -131,9 +176,13 @@ function TableSale({noComanda, saleDetail, deleteSaleDetail }) {
           Q.{platillo.precio.toFixed(2)}
         </td>
         <td>
-          Q.{totalPlatillo.toFixed(2)}
+          Q.{(cantidades[index] * platillo.precio).toFixed(2)}
           <button
-            onClick={() => handleDelete(platillo.idPlatillo)}
+            onClick={() => {
+              handleDelete(platillo.idPlatillo)
+              handleCantidadDelete(index)
+            }}
+              
           >
             <BsFillTrashFill 
               color="#FF0000"
@@ -145,7 +194,7 @@ function TableSale({noComanda, saleDetail, deleteSaleDetail }) {
       )}
     <tr>
       <th colSpan="5" className="footer-table">Total Comanda: </th>
-      <td>Q.350.00</td>
+      <td>Q.{totalComanda.toFixed(2)}</td>
     </tr>
    
   </tbody>
--- ClientApp/src/pages/IndividualSale.jsx
+++ ClientApp/src/pages/IndividualSale.jsx
@@ -1,123 +1,154 @@
-import React, { useState, useEffect } from 'react'
-import { useStore } from '../providers/GlobalProvider'
-import { FormGroup, Label, Col, Input } from 'reactstrap'
-import TableSale from '../components/TableSale'
-import '../styles/IndividualSale.scss'
-import SearchBarDrop from '../components/menu-search-drop/SearchBarDrop'
+import React, { useState, useEffect } from 'react';
+import { useStore } from '../providers/GlobalProvider';
+import { FormGroup, Label, Col, Input } from 'reactstrap';
+import TableSale from '../components/TableSale';
+import '../styles/IndividualSale.scss';
+import SearchBarDrop from '../components/menu-search-drop/SearchBarDrop';
+
 function IndividualSale(props) {
   useEffect(() => {
     // Para establecer en el módulo en el que nos encontramos
     props.setTitle("Nueva Venta");
   }, []);
-   /* isOpen (globalstate) -> para que el contenido se ajuste según el ancho de la sidebar (navegación) */
-  const isOpen = useStore((state) => state.sidebar)
+
+  /* isOpen (globalstate) -> para que el contenido se ajuste según el ancho de la sidebar (navegación) */
+  const isOpen = useStore((state) => state.sidebar);
   // DataApi
   const [menu, setMenu] = useState([]);
-  // 
+  //
   const [noDataMenu, setNoDataMenu] = useState(false);
-  const [numeroComanda, setNumeroComanda] = useState("")
+  const [numeroComanda, setNumeroComanda] = useState("");
   // Item seleccionado prop de <SearchBarDrop />
-  const [itemSelected, setItemSelected] = useState(null)
+  const [itemSelected, setItemSelected] = useState(null);
   // Platillos seleccionados
-  const [saleDetail, setSaleDetail] = useState([])
+  const [saleDetail, setSaleDetail] = useState([]);
+  // Cantidades del input del componente <TableSale/>
+  const [cantidades, setCantidades] = useState([]);
 
   // Identifica el item de <searchbardrop> seleccionado -> prop al componente nieto :v
   const setItemSelectedList = (id) => {
     // Sin repetidos: si el item ya existe en el array saleDetail, corta la secuencia de este bloque de código
-    if(saleDetail.find((item) => item.idPlatillo === id)) return
+    if (saleDetail.find((item) => item.idPlatillo === id)) return;
     // Busca los items que coincidan entre dataApi e ItemSelected
-    const selectedItem = menu.find((item) => item.idPlatillo === id)
-    console.log(selectedItem)
+    const selectedItem = menu.find((item) => item.idPlatillo === id);
+    console.log(selectedItem);
 
-    setItemSelected(id)
+    setItemSelected(id);
     // Agregamos al array el nuevo item, y además no se repite ningún item
-    setSaleDetail((prevSaleDetail) => [...prevSaleDetail, selectedItem])
-  }
-  
+    setSaleDetail((prevSaleDetail) => [...prevSaleDetail, selectedItem]);
+    // Inicializamos el valor en 1
+    setCantidades((prevCantidades) => [...prevCantidades, 1]);
+  };
+
   /* Eliminar un item de la venta */
   const deleteSaleDetail = (id) => {
     setSaleDetail((prevSaleDetail) => {
-      // si el id del elemento actual es diferente al id que se pasa como argumento
+      // Nuevos elementos, elimina los items que no coincidan con el id propocionado como parámetro
       const nuevosElementos = prevSaleDetail.filter((item) => item.idPlatillo !== id);
       return nuevosElementos;
     });
   };
 
- /*  useEffect(() => {
+  /*  useEffect(() => {
     console.warn('Final -->')
     console.log(saleDetail)
   }, [setItemSelectedList]) */
-  
+
   const getData = async () => {
     try {
       // if(!searchQuery || searchQuery.trim() === "") return
       // https://jsonplaceholder.typicode.com/comments
       // setLoading(true)
-      setNoDataMenu(false)
+      setNoDataMenu(false);
       // if(response.data && response.data.length === 0) setNoDataMenu(true)
       const response = await fetch('http://localhost:5173/api/Menu', {
         headers: {
-          'Authorization': `Bearer ${localStorage.token}`
-        }
-      })
-      const json = await response.json() 
-      setMenu(json)
-      console.log(json)
+          'Authorization': `Bearer ${localStorage.token}`,
+        },
+      });
+      const json = await response.json();
+      setMenu(json);
+      console.log(json);
+    } catch (err) {
+      console.error(err);
     }
-    catch(err) {
-      console.error(err)
-    }
-  }
+  };
 
   useEffect(() => {
-    getData()
-  }, [])
+    getData();
+  }, []);
 
   // console.log(menu)
   // Manejador del número de comanda
   const handleChange = (e) => {
-    e.preventDefault()
-    setNumeroComanda(e.target.value)
-  }
+    e.preventDefault();
+    setNumeroComanda(e.target.value);
+  };
+
+  // Actualiza la cantidad asociada al item agregado a la comanda, si el item 
+  const updateCantidades = (index, nuevaCantidad) => {
+    setCantidades((prevCantidades) => {
+      // Nuevas cantidades es una copia de prevCantidades [2,3,43 ... etc] cada número simboliza el value de cada input
+      const nuevasCantidades = [...prevCantidades];
+      // index representa la posición del elemento en el array de cantidades que queremos modificar.
+      nuevasCantidades[index] = nuevaCantidad;
+      // nuevasCantidades es el nuevo valor de la cantidad que se ha ingresado en el input
+      return nuevasCantidades;
+    });
+  };
+
+  const updateCantidadesDelete = (indexItem) => {
+    setCantidades((prevCantidades) => {
+      const cantidadesActualizadas = prevCantidades.filter((_, index) => index !== indexItem);
+      return cantidadesActualizadas;
+    });
+  };
 
   return (
-    <div className={ isOpen ? "wrapper" : "side" }>
+    <div className={isOpen ? "wrapper" : "side"}>
       <section className="comanda">
         <h2>Comanda digital</h2>
         <form className="comanda-form">
-        <div className="container">
-          <div className="row">
-            <div className="col-3">
-              <div className="form-floating mb-3">
-                <input type="number" className="form-control" id="input-comanda" placeholder="1332" onChange={handleChange}/>
-                <label htmlFor="input-comanda">No. de Comanda</label>
+          <div className="container">
+            <div className="row">
+              <div className="col-3">
+                <div className="form-floating mb-3">
+                  <input
+                    type="number"
+                    className="form-control"
+                    id="input-comanda"
+                    placeholder="1332"
+                    onChange={handleChange}
+                  />
+                  <label htmlFor="input-comanda">No. de Comanda</label>
+                </div>
+              </div>
+              <div className="col-9">
+                <SearchBarDrop
+                  itemSelected={itemSelected}
+                  setItemSelectedList={setItemSelectedList}
+                  menuState={menu}
+                  setNoDataMenu={setNoDataMenu}
+                  noDataMenu={noDataMenu}
+                />
               </div>
             </div>
-            <div className="col-9">
-              <SearchBarDrop 
-                itemSelected={itemSelected} 
-                setItemSelectedList={setItemSelectedList}
-                menuState={menu}
-                setNoDataMenu={setNoDataMenu}
-                noDataMenu={noDataMenu}
-              />
-            </div>
-          </div>          
-        </div>
+          </div>
         </form>
       </section>
       <section className="comanda-table">
-        <TableSale 
+        <TableSale
           noComanda={numeroComanda}
           /* Nuevo array con los items seleccionados */
+          cantidades={cantidades}
           saleDetail={saleDetail}
           deleteSaleDetail={deleteSaleDetail}
-          // setSaleDetail={}
-          // deleteSaleDetail={(id) => deleteSaleDetail(id)}
+          updateCantidades={updateCantidades}
+          updateCantidadesDelete={updateCantidadesDelete}
         />
       </section>
     </div>
-  )
+  );
 }
 
-export default IndividualSale
+export default IndividualSale;
