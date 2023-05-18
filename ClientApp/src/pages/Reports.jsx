680f7ce9823fe04e2f13edc0ed6e3a4bc8d1e03d
Danny <15daixca@gmail.com>
2023-05-18 05:01:15 -0600

    Reporte diario mejorado y falta semana y mes

--- ClientApp/src/pages/Reports.jsx
+++ ClientApp/src/pages/Reports.jsx
@@ -1,17 +1,217 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useStore } from '../providers/GlobalProvider'
-function Reports(props) {
-  /* isOpen (globalstate) -> para que el contenido se ajuste según el ancho de la sidebar (navegación) */
+import DatePicker from '../components/DatePicker';
+import Searchbar from '../components/Searchbar';
+import Select from '../components/Select';
+import TableData from '../components/TableData';
+import ButtonDrop from '../components/ButtonDrop';
+import { Col, Button, Label, Input, } from 'reactstrap'
+// import { Row, Col,  Button } from "reactstrap";
+import { FcPrint } from 'react-icons/fc';
+import ModalNewSale from '../components/modales/ModalNewSale';
+// function Reports(props) {
+//   /* isOpen (globalstate) -> para que el contenido se ajuste según el ancho de la sidebar (navegación) */
+//   const isOpen = useStore((state) => state.sidebar)
+  
+
+//   //****
+//   const [selectedDate, setSelectedDate] = useState('');
+
+//   const handleDateChange = (event) => {
+//     setSelectedDate(event.target.value);
+//   };
+
+
+//   const handleGeneratePdf = async () => {
+//     try {
+//       const response = await fetch(`http://localhost:5173/api/generate-pdf?date=${selectedDate}`, {
+//         method: 'GET',
+//         responseType: 'blob', // Importante: establecer el tipo de respuesta como blob
+//       });
+
+//       // Obtener el contenido del PDF
+//       const pdfBlob = await response.blob();
+
+//       // Crear un objeto URL del blob
+//       const pdfUrl = URL.createObjectURL(pdfBlob);
+
+//       // Abrir el PDF en una nueva ventana o pestaña del navegador
+//       window.open(pdfUrl, '_blank');
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+//   //*/
+//   return (
+//     <div>
+//     <label htmlFor="dateInput">Fecha:</label>
+//     <input type="date" id="dateInput" value={selectedDate} onChange={handleDateChange} />
+//     <button onClick={handleGeneratePdf}>Generar PDF</button>
+//   </div>
+//   )
+// }
+
+
+
+// export default Reports
+
+
+
+function Reports (props)  {
   const isOpen = useStore((state) => state.sidebar)
   useEffect(() => {
     // Para establecer en el módulo en el que nos encontramos
     props.setTitle("Reportes");
   });
-  return (
-    <div className={ isOpen ? "wrapper" : "side" }>
-      <h1>Reports</h1>
-    </div>
-  )
-}
+  //   /* isOpen (globalstate) -> para que el contenido se ajuste según el ancho de la sidebar (navegación) */
+  // const isOpen = useStore((state) => state.sidebar)
+  const [seledia, setseledia] = useState([]);
 
-export default Reports
+  const ManejoCambioFecha = (event) => {
+    setseledia(event.target.value);
+  };
+
+  // const handleGeneratePdf = () => {
+  //   const url = `http://localhost:5173/api/pdf?date=${selectedDate}`;
+  //   fetch(url, {
+  //     method: 'GET',
+  //     headers: {
+  //       'Content-Type': 'application/pdf',
+  //     },
+  //     responseType: 'blob',
+  //   })
+  //     .then((response) => response.blob())
+  //     .then((blob) => {
+  //       const url = window.URL.createObjectURL(blob);
+  //       const link = document.createElement('a');
+  //       link.href = url;
+  //       // window.open(blob, '_blank');
+  //       link.setAttribute('dowload', 'ventas.pdf');
+  //       document.body.appendChild(link);
+  //       link.click();
+  //       link.remove();
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error:', error);
+  //     });
+  // };
+
+  const generarPdf = () => {
+    const url = `http://localhost:5173/api/pdf?date=${seledia}`;
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/pdf',
+      },
+      responseType: 'blob',
+    })
+      .then((response) => response.blob())
+      .then((blob) => {
+        const url = window.URL.createObjectURL(blob);
+        const newWindow = window.open(url, '_blank');
+        if (!newWindow) {
+          throw new Error('No se pudo abrir el PDF en una pestaña nueva.');
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
+    /* ----- Buscador */
+  // state para buscador
+  // const [search, setSearch] = useState("")
+  // // buscador, captura de datos
+  // const searcher = (e) => {
+  //   console.log(e.target.value)
+  //   setSearch(e.target.value)
+  //   console.log(e.target.value.length)
+  // }
+  // //metodo de filtrado del buscador
+  // // Si state search es null (no sea ha ingresado nada en el input) results = dataApi
+  // const results = !search ? dataApi 
+  // // Si se ha ingresado información al input, que la compare a los criterios
+  // : dataApi.filter((item) =>
+  //   item.email.toLowerCase().includes(search.toLocaleLowerCase())
+  // )
+// FIN BUSCAR
+
+
+
+
+  
+
+  
+  return (
+
+
+
+  //   <Row className="justify-content-center">
+  //   <Col xs="auto">
+  //     <div>
+  //       <Label htmlFor="dateInput">Fecha:</Label>
+  //       <Input
+  //         type="date"
+  //         id="dateInput"
+  //         value={seledia}
+  //         onChange={ManejoCambioFecha}
+  //       />
+  //       <Button onClick={generarPdf}>Generar PDF</Button>
+  //     </div>
+  //   </Col>
+  // </Row>
+
+  <div className={ isOpen ? "wrapper" : "side" }>
+      <div className="container-fluid mt-4">
+        <div className="row">
+          {/* <div className="col">
+            <DatePicker />
+          </div> */}
+          <div className="col">
+            {/* <Searchbar searcher={searcher}/> */}search
+          </div>
+        </div>
+        <div className="row d-flex justify-content-center align-items-center">
+          <div className="col">
+            <Select />
+          </div>
+        
+        <Col className="border p-3" >
+            {/* <ButtonDrop>
+              <FcPrint />
+            </ButtonDrop> */}
+            <Label htmlFor="dateInput">Seleccionar fecha:</Label>
+            <Input
+              type="date"
+              id="dateInput"
+              value={seledia}
+              onChange={ManejoCambioFecha}
+            />
+            <Button
+              onClick={generarPdf}
+              color="primary"
+              outline
+            >
+              Generar reporte
+              <FcPrint />
+            </Button>
+          </Col>
+        
+          
+        </div>
+        <div className="row">
+          <div className="col">
+            {/* <TableData data={results} /> */}TableData
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+    
+  );
+};
+
+export default Reports;
--- Controllers/pdfController.cs
+++ Controllers/pdfController.cs
@@ -28,25 +28,31 @@ namespace restaurante_web_app.Controllers
         }
 
         [HttpGet]
-        public IActionResult GeneratePdf()
+        //[HttpGet("generate-pdf")]
+        public IActionResult GeneratePdf([FromQuery] DateTime date)
         {
 
+            string openSansFontPath = "https://fonts.googleapis.com/css?family=Roboto"; // Reemplaza con la ruta real del archivo de fuente
+
+
+            DateOnly selectedDate = DateOnly.FromDateTime(date);
+
+            DateOnly today = DateOnly.FromDateTime(DateTime.Today);
+            //DateOnly today = DateOnly.FromDateTime(new DateTime(2023, 3, 3));
+
             
+            // Obtener el nombre del usuario
+            //string userId = User.Identity.Name; // Obtén el nombre del usuario autenticado
+            //var usuario = _dbContext.Usuarios.FirstOrDefault(u => u.Usuario1 == userId);
+            //string nombreUsuario = usuario != null ? usuario.Usuario1 : "Nombre de usuario desconocido";
 
-            DateTime today = DateTime.Today;
-            DateTime weekStart = today.AddDays(-(today.DayOfWeek - DayOfWeek.Monday + 7) % 7);
-            DateTime weekEnd = weekStart.AddDays(6);
 
-            // Convert to DateOnly
-            DateOnly weekStartDate = DateOnly.FromDateTime(weekStart);
-            DateOnly weekEndDate = DateOnly.FromDateTime(weekEnd);
-
-            // Get the data from the database for the current week
+            // Obtener los datos de las tablas de la base de datos solo para hoy
             var data = from c in _dbContext.Clientes
                        join v in _dbContext.Ventas on c.IdCliente equals v.IdCliente
                        join d in _dbContext.DetalleVenta on v.IdVenta equals d.IdVenta
                        join m in _dbContext.Menus on d.IdPlatillo equals m.IdPlatillo
-                       where v.Fecha >= weekStartDate && v.Fecha < weekEndDate
+                       where v.Fecha == selectedDate
                        select new
                        {
                            v.IdVenta,
@@ -65,11 +71,20 @@ namespace restaurante_web_app.Controllers
 
             decimal totalVentas = data.Sum(item => item.Subtotal ?? 0);
 
+
+
+            // Group the data by customer
+            //var groupedData = data.GroupBy(item => item.IdCliente);
+
+
+
+
             // Group the data by customer
             var groupedData = data.GroupBy(item => item.IdCliente);
 
-            // Calculate the total sales for the week
-            //decimal totalVentas = groupedData.Sum(group => group.Sum(item => item.Subtotal));
+            //suma de hoy
+            //decimal totalVentas = groupedData.Sum(group => group.Sum(item => item.Subtotal ?? 0));
+
 
             // Create a PDF document
             Document document = new Document();
@@ -78,74 +93,152 @@ namespace restaurante_web_app.Controllers
             MemoryStream memoryStream = new MemoryStream();
             PdfWriter writer = PdfWriter.GetInstance(document, memoryStream);
 
+
             // Add metadata
             document.AddTitle("Facturas de ventas");
             document.AddSubject("Facturas de ventas de la base de datos");
             document.AddKeywords("ventas, base de datos, PDF");
             document.AddCreator("Mi aplicación");
 
-            // Define the default font style
+            // Definir el estilo de fuente predeterminado
             //FontFactory.Register(openSansFontPath, "Roboto Mono");
 
             // Open the PDF document
             document.Open();
 
+            // Crear el encabezado
+            PdfPTable headerTable = new PdfPTable(3);
+            headerTable.WidthPercentage = 100;
+
+            // Agregar la imagen al encabezado
+            string imagePath = "https://umg.edu.gt/miumg/sesion_files/logo_white.png"; // Reemplaza con la ruta de tu imagen
+            Image image = Image.GetInstance(imagePath);
+            image.ScaleAbsolute(100, 100); // Ajustar el tamaño de la imagen
+            PdfPCell imageCell = new PdfPCell(image);
+            imageCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            imageCell.Border = iTextSharp.text.Rectangle.NO_BORDER;
+            headerTable.AddCell(imageCell);
+
+            // Agregar el título al encabezado
+            //PdfPCell titleCell = new PdfPCell(new Phrase("Título del documento"));
+            PdfPCell titleCell = new PdfPCell(new Phrase("Cafe y Restaurante La Centenaria", new Font(Font.FontFamily.COURIER, 16, Font.BOLD))); 
+            titleCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            titleCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            titleCell.Border = iTextSharp.text.Rectangle.NO_BORDER;
+            headerTable.AddCell(titleCell);
+
+
+            // Obtener la fecha de hoy
+            DateTime daynow = DateTime.Now;
+            string fechaHoy = daynow.ToString();
+
+            // Agregar la descripción al encabezado
+            PdfPCell descriptionCell = new PdfPCell(new Phrase($"Nombre: {nombreUsuario}\nReporte Diario de Ventas\n\nFecha de Impresión:\n{fechaHoy}", new Font(Font.FontFamily.COURIER, 10, Font.NORMAL)));
+            descriptionCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            descriptionCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            descriptionCell.Border = iTextSharp.text.Rectangle.NO_BORDER;
+            headerTable.AddCell(descriptionCell);
+
+            // Agregar el encabezado al documento
+            document.Add(headerTable);
+
             // Loop through the groups and add a new page for each customer
             foreach (var group in groupedData)
             {
+
+                // Linea separador
+                LineSeparator line = new LineSeparator();
+                line.LineColor = BaseColor.BLACK; // Color de la línea
+                line.LineWidth = 1f; // Ancho de la línea
+                // Agregar la línea al documento
+                document.Add(new Chunk(line));
+
                 // Add the customer details to the page
-                Paragraph customer = new Paragraph($"Cliente: {group.Key}, {group.First().Institucion}, {group.First().NombreApellido}, {group.First().Fecha}");
+                Paragraph customer = new Paragraph($"Cliente: {group.First().NombreApellido} \n" +
+                                                   $"Intitución: {group.First().Institucion} \n" +
+                                                   $"Fecha de Registro: {group.First().Fecha} \n\n", new Font(Font.FontFamily.COURIER, 12, Font.BOLD));
                 customer.Alignment = Element.ALIGN_LEFT;
                 document.Add(customer);
+
+
+
                 // Add the sale items
                 PdfPTable table = new PdfPTable(4);
                 table.WidthPercentage = 100;
                 table.SetWidths(new float[] { 1f, 1f, 1f, 1f });
                 table.DefaultCell.Padding = 5;
                 table.DefaultCell.BackgroundColor = new BaseColor(240, 240, 240);
-                table.AddCell("Platillo");
-                table.AddCell("Cantidad");
-                table.AddCell("Precio");
-                table.AddCell("Subtotal");
+
+                // Fuente para el encabezado (negrita)
+                Font headerFont = new Font(Font.FontFamily.COURIER, 12, Font.BOLD);
+
+                // Encabezado de la tabla en negrita
+                PdfPCell headerCell = new PdfPCell(new Phrase("Platillo", headerFont));
+                table.AddCell(headerCell);
+                headerCell = new PdfPCell(new Phrase("Cantidad", headerFont));
+                table.AddCell(headerCell);
+                headerCell = new PdfPCell(new Phrase("Precio", headerFont));
+                table.AddCell(headerCell);
+                headerCell = new PdfPCell(new Phrase("Subtotal", headerFont));
+                table.AddCell(headerCell);
+
+                // Fuente para el contenido de la tabla (normal)
+                Font contentFont = new Font(Font.FontFamily.COURIER, 12, Font.NORMAL);
+
                 foreach (var item in group)
                 {
-                    table.AddCell(item.Platillo);
-                    table.AddCell(item.Cantidad.ToString());
-                    table.AddCell(item.Precio.ToString());
-                    table.AddCell(item.Subtotal.ToString());
+                    table.AddCell(new PdfPCell(new Phrase(item.Platillo, contentFont)));
+                    table.AddCell(new PdfPCell(new Phrase(item.Cantidad.ToString(), contentFont)));
+                    table.AddCell(new PdfPCell(new Phrase(item.Precio.ToString(), contentFont)));
+                    table.AddCell(new PdfPCell(new Phrase(item.Subtotal.ToString(), contentFont)));
                 }
+
                 // Add the table to the document
                 document.Add(table);
+
                 // Add the total
-                Paragraph total = new Paragraph($"Total: {group.Sum(item => item.Subtotal).ToString()}");
+                Paragraph total = new Paragraph($"Total: {group.Sum(item => item.Subtotal).ToString()}", contentFont);
                 total.Alignment = Element.ALIGN_RIGHT;
                 document.Add(total);
+
+
+
+
             }
 
-            // Add the total sales for the week
-            Paragraph totalSales = new Paragraph($"Total de ventas de la semana: {totalVentas.ToString("C")}");
+            //linea separador
+            LineSeparator line3 = new LineSeparator();
+            line3.LineColor = BaseColor.BLUE; // Color de la línea
+            line3.LineWidth = 1f; // Ancho de la línea
+                                  // Agregar la línea al documento
+            document.Add(new Chunk(line3));
+
+            // Obtener el nombre del día en español
+            CultureInfo spanishCulture = new CultureInfo("es-ES");
+            string dayOfWeek = spanishCulture.DateTimeFormat.GetDayName(selectedDate.DayOfWeek);
+
+            // Add the total sales for the day
+            Paragraph totalSales = new Paragraph($"Total de ventas del día {dayOfWeek}: {totalVentas.ToString("C")}", new Font(Font.FontFamily.COURIER, 14, Font.BOLD));
             totalSales.Alignment = Element.ALIGN_RIGHT;
             document.Add(totalSales);
 
             // Close the document
             document.Close();
 
-
-
             // Convert the PDF document to an array of bytes
             byte[] bytes = memoryStream.ToArray();
 
             // Prepare the HTTP response
-            var result = new FileContentResult(bytes, "application/pdf");
+            //var result = new FileContentResult(bytes, "application/pdf");
 
-            // Liberar recursos
-            writer.Close();
-            memoryStream.Close();
-            memoryStream.Dispose();
+            //// Liberar recursos
+            //writer.Close();
+            //memoryStream.Close();
+            //memoryStream.Dispose();
 
-            return result;
+            return File(bytes, "application/pdf"); ;
+
 
         }
     }
-    }
-
+}
